65144b0c1a353f0c93df4898f98599d906238988
OktiWolfik <makarbagiev200199@gmail.com>
2022-07-05 16:46:52 +0300

    commit on '1-feat-storage-table'

---
+++ pages/table.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="desserts"
+    :items-per-page="5"
+    class="elevation-1"
+  ></v-data-table>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        headers: [
+          {
+            text: 'Наименование',
+            align: 'start',
+            sortable: false,
+            value: 'package',
+          },
+          { text: 'Опция', value: 'Number' },
+          { text: 'Количество', value: 'quantity' },
+          { text: 'Поставщик', value: 'supplier' },
+          { text: 'Сумма', value: 'total' },
+          { text: 'Дата', value: 'date' },
+        ],
+        desserts: [
+          {
+            package: 'Сахар-песок',
+            Number: 'поступление',
+            quantity: 24,
+            supplier: 'ИП Иванов',
+            total: '15000 руб.',
+            date: '13.06.2022'
+          },
+          {
+            package: 'Манная крупа',
+            Number: 'возврат',
+            quantity: 37,
+            supplier: 'Родные края',
+            total: '13500 руб.',
+            date: '05.07.2022'
+          },
+          {
+            package: 'губка для кухни',
+            Number: 'отгрузка',
+            quantity: 23,
+            supplier: 'Каждый день',
+            total: '14000 руб.',
+            date: '01.07.2022'
+          },
+        ],
+      }
+    },
+  }
+</script>
